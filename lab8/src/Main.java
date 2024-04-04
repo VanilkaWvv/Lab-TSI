6122487fafbd555298565306ab046bea98f58bf6
VanilkaWvv <valentinprepelita3@gmail.com>
2024-04-04 13:33:55 +0300

    Labul 8 fix bug

--- lab8/src/Main.java
+++ lab8/src/Main.java
@@ -52,6 +52,7 @@ public class Main {
                 inputText.setText("");
                 keyText.setText("");
                 keyIntText.setText("");
+                inputText.setEditable(true);
             }
         });
 
